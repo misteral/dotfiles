06d873f2826c5c04885cefbe3d3ddf9d52f817bd
Alexander Bobrov <al.bobrov.mail@gmail.com>
2017-07-13 11:04:40 +0500

    Merge with current config

---
+++ .config/fish/conf.d/my_aliases.fish
@@ -0,0 +1,15 @@
+# alias gco 'git checkout'
+# alias be 'bundle exec'
+# alias gc 'git commit -m'
+# alias gst 'git status'
+# alias ga 'git add'
+alias g 'git'
+alias s 'subl .'
+# alias gb 'git branch'
+
+alias pgstart 'pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+
+alias fmrelax 'mplayer -cache 800 "http://ic8.101.ru:8000/v13_1?userid=0&setst=28r2t8f0bgusnp17p0k50sb2g5&city=0"'
+alias fmnature 'mplayer -cache 800 "http://ic8.101.ru:8000/c19_1?userid=0&setst=b7bdcbql0rt7ap9e9qbk753ot2&tok=21111963qrfrVY2A%2Br3XLlkqadZ0pw%3D%3D3&city=0"'
+alias fmchillout 'mplayer -cache 800 "http://ic7.101.ru:8000/c15_3"'
+alias fmnashe 'mplayer -cache 800 'http://nashe1.hostingradio.ru/nashe-128.mp3''
